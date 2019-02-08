42153bd6f94c70a6f934f578b1260fcdad40ce34
anniemao <amao9098@gmail.com>
2019-02-08 12:12:39 -0800

    Main page outline. Need to covert to kv files.

--- view.py
+++ view.py
@@ -1,49 +1,56 @@
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
-from kivy.garden.navigationdrawer import NavigationDrawer
+from kivy.uix.dropdown import DropDown
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
 from kivy.metrics import dp
+from kivy.base import runTouchApp
 
 
 class MainScreen:
 
     def __init__(self):
-        self.navigationdrawer = NavigationDrawer()
+        self.drop_down_menu = DropDown()
 
     def build(self):
-        side_panel = BoxLayout(orientation='vertical')
-        side_panel.add_widget(Label(text='Myovate Menu'))
-        #side_panel.add_widget(Button(text='Back'))
-        back_button = Button(text='Back')
-        callibration_button = Button(text='Callibration')
-        back_button.bind(on_press=lambda j: self.navigationdrawer.toggle_state())
-        progress_button = Button(text='Progress')
-        side_panel.add_widget(back_button)
-        side_panel.add_widget(callibration_button)
-        side_panel.add_widget(progress_button)
-        self.navigationdrawer.add_widget(side_panel)
-        self.navigationdrawer.toggle_main_above()
-        self.navigationdrawer.toggle_state()
-        self.navigationdrawer.anim_type = 'slide_above_simple'
+        # Main menu text
+        myovate_menu_text = "Myovate Menu"
 
-        label_head = (
-            'MYOVATE')
-        main_panel = BoxLayout(orientation='vertical')
-        label_bl = BoxLayout(orientation='horizontal')
-        label = Label(text=label_head, font_size='24sp',
-                      markup=True, halign='center', valign='top')
-        label_bl.add_widget(Widget(size_hint_x=None, width=dp(10)))
-        label_bl.add_widget(label)
-        label_bl.add_widget(Widget(size_hint_x=None, width=dp(10)))
-        main_panel.add_widget(Widget(size_hint_y=None, height=dp(10)))
-        main_panel.add_widget(label_bl)
-        main_panel.add_widget(Widget(size_hint_y=None, height=dp(10)))
-        self.navigationdrawer.add_widget(main_panel)
+        # Layout
+        layout_drop = AnchorLayout(anchor_x='left', anchor_y='top')
+        layout_myovate = AnchorLayout(anchor_x='center', anchor_y='center')
+        layout_main = FloatLayout()
 
+        # Welcome text
+        welcome_text = "Welcome to Myovate"
+        welcome_label = Label(text=welcome_text, font_size='40sp')
+        layout_myovate.add_widget(welcome_label)
 
-        return self.navigationdrawer
+        # Make buttons
+        calibration_button = Button(text='Calibration', size_hint_y=None, height=150)
+        calibration_button.bind(on_release=lambda btn: self.drop_down_menu.select(myovate_menu_text))
+
+        progress_button = Button(text='Progress', size_hint_y=None, height=150)
+        progress_button.bind(on_release=lambda btn: self.drop_down_menu.select(myovate_menu_text))
+
+        # Add buttons to layout
+        self.drop_down_menu.add_widget(calibration_button)
+        self.drop_down_menu.add_widget(progress_button)
+
+        # Menu button
+        menu_button = Button(text=myovate_menu_text, size_hint=(0.2, 0.2))
+        menu_button.bind(on_release=self.drop_down_menu.open)
+        self.drop_down_menu.bind(on_select=lambda instance, x: setattr(menu_button,'text', x))
+        layout_drop.add_widget(menu_button)
+
+        #Add to main layout
+        layout_main.add_widget(layout_drop)
+        layout_main.add_widget(layout_myovate)
+
+        return runTouchApp(layout_main)
 
 
 
