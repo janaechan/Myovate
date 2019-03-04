9e76217ab65ec27cf72a944444a85c125499130b
anniemao <amao9098@gmail.com>
2019-03-03 20:50:14 -0800

    arduino comm test

--- Arduino.py
+++ Arduino.py
@@ -1,25 +1,34 @@
 import serial
-
+import warnings
+import serial.tools.list_ports
 
 class Arduino():
 
     def __init__(self):
-        self.baud_rate = 0
-        self.serial_object = ""
+        self.baud_rate = 9600
         self.data_points = 120
         self.cal_points = 70
         self.arduino = None
         self.cal = {}
+        self.ser = None
+        self.arduino_ports = None
 
-    def set_arduino(self, serial_id, baud_rate):
-        self.serial_object = serial_id
-        self.baud_rate = baud_rate
-        self.arduino = serial.Serial(self.serial_object, self.baud_rate, timeout=0.1)
+    def find_arduino(self):
+        self.arduino_ports = [
+            p.device
+            for p in serial.tools.list_ports.comports()
+            if 'Arduino' in p.description
+        ]
+        return self.arduino_ports
+
+    def set_arduino(self, ser):
+        self.ser = ser
+        self.arduino = serial.Serial(self.ser, self.baud_rate, timeout=0.1)
 
     def low_calibration(self, channel_num):
         final_data = []
         while len(final_data) < self.data_points:
-            data = self.arduino.readline()
+            data = self.arduino.readline().split(',')
             final_data.append(data[channel_num])
         final_data.sort()
         low_cal = sum(final_data[0:self.cal_points])/self.cal_points
@@ -29,7 +38,7 @@ class Arduino():
     def high_calibration(self, channel_num):
         final_data = []
         while len(final_data) < self.data_points:
-            data = self.arduino.readline()
+            data = self.arduino.readline().split(',')
             final_data.append(data[channel_num])
         final_data.sort()
         high_cal = sum(final_data[0:self.cal_points])/self.cal_points
@@ -37,3 +46,16 @@ class Arduino():
         self.arduino.write(str(channel_num) + "," + str(self.cal[channel_num[0]]) + "," + str(high_cal))
         return high_cal
 
+    def test_comm(self):
+        self.arduino.write('4')
+        print(self.arduino.readline())
+
+    if __name__ == '__main__':
+        all_ar = find_arduino()
+        if (len(all_ar) < 1):
+            print("No Arduino found")
+        else:
+            if (len(all_ar) > 1):
+                print("Multiple Arduino")
+            set_arduino(all_ar[0])
+            test_comm()
