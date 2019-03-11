4af732c2045811f8fd3a0cda47782496694c9aeb
Janae Chan <jchan15@cs.washington.edu>
2019-03-10 22:26:10 -0700

    push

--- NewMyovate.py
+++ NewMyovate.py
@@ -19,10 +19,7 @@ import MainScreen
 import ProgressScreen
 import MicrophoneThread
 import GraphThread
-<<<<<<< HEAD
 import Arduino
-=======
->>>>>>> c739216ded632301e165d9068c2672b59a7b27f6
 import Misc
 
 # class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior,
@@ -44,18 +41,9 @@ if __name__ == '__main__':
     # TODO create arduino instance
     # TODO start arduino thread when you start new session so you can call find_, set_, get_data
     # levels = []  # store levels of microphone
-<<<<<<< HEAD
     # get_microphone_thread = Thread(target=MicrophoneThread.get_microphone_level)
     # get_microphone_thread.start()
     # get_arduino_thread = Thread(target=GraphThread.get_data_test)
     Window.fullscreen = 'auto'
-=======
-
-    # get_arduino_thread = Thread(target=GraphThread)
-    # get_arduino_thread.daemon = True
-    # get_arduino_thread.start()
-    # GraphThread.get_data_test()
-    # Window.fullscreen = 'auto'
->>>>>>> c739216ded632301e165d9068c2672b59a7b27f6
     NewMyovateApp().run()
     # arduino = Arduino.Arduino()
